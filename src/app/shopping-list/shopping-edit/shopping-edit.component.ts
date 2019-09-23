7f72aaa192ebbc86757507cdeeab229a7b8b7479
MonaliSamarth <manalis@winjit.com>
2019-09-23 18:09:51 +0530

    Loading the Shopping List Items into the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -11,12 +11,14 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm: NgForm;
   // @ViewChild('nameInput',{static:false}) nameInputReference: ElementRef;
   // @ViewChild('amountInput',{static:false}) amountInputReference: ElementRef;
   // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient
 
   constructor(private recipeList: ShoppingListService) { }
 
@@ -26,6 +28,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         (index: number) => {
           this.editedItemIndex = index;
           this.editMode = true;
+          this.editedItem = this.recipeList.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
         }
       );
   }
