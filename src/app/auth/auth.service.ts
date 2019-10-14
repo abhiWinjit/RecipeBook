0e9741c930779905e2e598a5c7d59d475a58f08e
abhijeeta <abhijeeta@winjit.com>
2019-10-14 13:10:55 +0530

    SignUp and Login with validation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs'
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -15,12 +18,51 @@ export class AuthService {
     constructor(private http: HttpClient) { }
 
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAW2_E-hZSgmjKAfEOjxMhyr4vSdiN5_8s',
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAW2_E-hZSgmjKAfEOjxMhyr4vSdiN5_8s',
             {
                 email: email,
                 password: password,
                 returnSecureToken: true
             }
-        )
+        ).pipe(catchError(this.handleError)
+        );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAW2_E-hZSgmjKAfEOjxMhyr4vSdiN5_8s',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unkown error occured!';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exists';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'Your password is invalid';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'You are disabled for this site contact website owner';
+                break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMessage = 'You are blocked beacause of many attempts';
+                break;
+
+        }
+        return throwError(errorMessage);
     }
 }
