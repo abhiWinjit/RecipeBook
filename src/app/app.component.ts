e17316fb8afd318af66b465653269f8baa78e119
abhi@Winjit <abhijeeta@winjit.com>
2019-10-15 11:22:03 +0530

    Auto Login implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // title = 'assignment3';
 
   // loadedFeature = 'recipe';
   // onNavigation(feature2: string){
   //   this.loadedFeature = feature2;
   // }
+  constructor(private authService: AuthService) { }
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -47,6 +47,7 @@ export class AuthService {
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     login(email: string, password: string) {
@@ -60,6 +61,28 @@ export class AuthService {
         ).pipe(catchError(this.handleError), tap(resData => {
             this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
         }));
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
     }
 
     logout() {
